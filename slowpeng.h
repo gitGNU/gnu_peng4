eca53cf932129477b8d83802faa932c1ec50fe55
Klaus-J. Wolf <yanestra@users.sf.net>
2015-04-09 23:49:46 +0200

    complete (working snapshot)

--- slowpeng.h
+++ slowpeng.h
@@ -9,10 +9,16 @@ struct pengset
 
 struct pengpipe
 {
-    struct pengset **mtx;     /* 2d matrix, null terminated */
+    unsigned rounds;       /* y dimension */
+    unsigned variations;   /* x dimension */
+    struct pengset **mtx;     /* 2d matrix */
 };
 
 
 
 struct pengset *genpengset(unsigned blksize, struct mersennetwister *mt);
+struct pengpipe *genpengpipe(unsigned blksize, unsigned rounds, unsigned variations, struct mersennetwister *mt);
+void destroypengset(struct pengset *p);
+void destroypengpipe(struct pengpipe *p);
 void execpengset(struct pengset *p, const unsigned char *buf1, unsigned char *tmpbuf, unsigned char *buf2, char encrypt);
+void execpengpipe(struct pengset *p, const unsigned char *buf1, unsigned char *tmpbuf, unsigned char *buf2, char encrypt);
---
+++ whirlpool.h
@@ -0,0 +1,31 @@
+/*
+ * whirlpool.hh
+ */
+
+
+#define WHIRLPOOL_DIGESTBYTES 64
+#define WHIRLPOOL_DIGESTBITS  (8*WHIRLPOOL_DIGESTBYTES) /* 512 */
+
+#define WHIRLPOOL_WBLOCKBYTES 64
+#define WHIRLPOOL_WBLOCKBITS  (8*WHIRLPOOL_WBLOCKBYTES) /* 512 */
+
+#define WHIRLPOOL_LENGTHBYTES 32
+#define WHIRLPOOL_LENGTHBITS  (8*WHIRLPOOL_LENGTHBYTES) /* 256 */
+
+
+struct whirlpool
+{
+/* private: */
+    uint8_t  bitLength[WHIRLPOOL_LENGTHBYTES]; /* global number of hashed bits (256-bit counter) */
+    uint8_t  buffer[WHIRLPOOL_WBLOCKBYTES];    /* buffer of data to hash */
+    int      bufferBits;             /* current number of bits on the buffer */
+    int      bufferPos;              /* current (possibly incomplete) byte slot on the buffer */
+    uint64_t hash[WHIRLPOOL_DIGESTBYTES/8];    /* the hashing state */
+};
+
+
+struct whirlpool *whirlpool_processbuffer(void);
+void whirlpool_init(struct whirlpool *wp);
+void whirlpool_add(struct whirlpool *wp, const unsigned char * const source, unsigned long sourceBits);
+void whirlpool_finalize(struct whirlpool *wp, unsigned char * const result);
+void whirlpool_destroy(struct whirlpool *wp);
