423af26d625dd09882202900368b1d4075353b23
Klaus-J. Wolf <yanestra@users.sf.net>
2015-05-19 02:05:19 +0200

    docs, tarball

--- scripts/mktarball.sh
+++ scripts/mktarball.sh
@@ -8,3 +8,11 @@ mkdir $DIR
 tar -cf - "$@" |tar -xf - -C $DIR
 tar -cjf $ARC $DIR
 rm -fr $DIR
+
+if [ "$USER" = "kj" ]
+then
+ gpg -u D218DF91 -b $ARC
+ scp $ARC* yanestra@dl.sv.gnu.org:/releases/peng4/
+fi
+
+mv $ARC* tarball
